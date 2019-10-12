a7ed0f4490506569528b4ce4499e24bff8852725
yuekai <yuekai@kibey.com>
2019-10-12 09:05:18 +0800

    test2

--- test2.go
+++ test2.go
@@ -1,23 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
 func main() {
-	//url := "http://cartoon.qiniu.tblinker.com/2_d6d210_cover.jpg"
-	//url := "http://cartoon1.qiniu.tblinker.com/cd0dce/41d0e299.data"
-	url := "http://cartoon1.qiniu.tblinker.com/e034fb/703686/c9f0f895.data"
-	idx := strings.Index(url,".com/")
+	slice := []int{0,1,2,3}
+	m := make(map[int]*int)
 
-	substr := url[idx+5:]
-	arr := strings.Split(substr, "/")
-
-	var name string
-	for _,v := range arr {
-		name += "/"+v
+	for key,val := range slice {
+		m[key] = &val
 	}
-	name = name[1:]
-	fmt.Println(name)
+	ls := len(slice)
+	i:=0
+	for i= 0;i<ls;i++{
+		fmt.Println(slice[i])
+	}
+
+
+	for k,v := range m {
+		fmt.Println(k,"->",*v)
+	}
 }
